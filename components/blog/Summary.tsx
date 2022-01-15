9ca07c1105c96aa16f193cbbcbfd2cdb162e44d0
Joseph Mukorivo <josemukorivo@gmail.com>
2022-01-15 15:29:52 +0200

    fix: navbar height

--- components/blog/Summary.tsx
+++ components/blog/Summary.tsx
@@ -8,10 +8,10 @@ export const BlogSummary = ({ title, date, slug, readTime, coverImage }) => {
     >
       <Box className='absolute dark:text-white inset-0 flex flex-col justify-between p-3 py-4 md:p-5'>
         <Box className='flex justify-between 2xl:mt-5'>
-          <Text className='text-slate-600 text-xs md:text-base dark:text-slate-400 font-medium'>
+          <Text className='text-slate-600 text-sm md:text-base dark:text-slate-400 font-medium'>
             {readTime} min read
           </Text>
-          <Text className='text-slate-600 text-xs md:text-base dark:text-slate-400 font-medium'>
+          <Text className='text-slate-600 text-sm md:text-base dark:text-slate-400 font-medium'>
             {new Date(date).toLocaleDateString('en-US', {
               month: 'long',
               day: 'numeric',
@@ -19,7 +19,7 @@ export const BlogSummary = ({ title, date, slug, readTime, coverImage }) => {
             })}
           </Text>
         </Box>
-        <Text className='text-base md:text-3xl 2xl:text-4xl dark:text-white font-medium mb-0'>
+        <Text className='text-base md:text-3xl 2xl:text-4xl dark:text-white font-extrabold mb-0'>
           {title}
         </Text>
       </Box>
