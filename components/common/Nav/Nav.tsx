f705c2bd2da5cd797bb268316ddff0439270ef4d
Joseph Mukorivo <josemukorivo@gmail.com>
2022-01-18 15:06:32 +0200

    feat: light bg on dark mode

--- components/common/Nav/Nav.tsx
+++ components/common/Nav/Nav.tsx
@@ -2,7 +2,7 @@ import { Link, ToggleDark } from '@components/ui';
 
 export const Nav = () => {
   return (
-    <nav className='px-4 py-4 md:hidden flex justify-between items-center border-b dark:border-slate-700'>
+    <nav className='px-4 py-4 md:hidden flex justify-between items-center border-b dark:border-slate-800'>
       <Link
         href='/'
         className='text-rose-500 text-3xl font-heading font-medium transform hover:text-rose-600 dark:text-rose-500'
