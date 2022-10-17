c580f51744490a82c64375f9425a7160c06c494d
Joseph Mukorivo <josemukorivo@gmail.com>
2022-10-17 08:44:44 +0200

    get fresh articles

--- pages/index.tsx
+++ pages/index.tsx
@@ -64,7 +64,7 @@ export default function Home({ articles }) {
 export async function getStaticProps() {
   const { NEXT_PUBLIC_DEV_TO_USERNAME } = process.env;
   const res = await fetch(
-    `https://dev.to/api/articles?username=${NEXT_PUBLIC_DEV_TO_USERNAME}&per_page=5&state=all`
+    `https://dev.to/api/articles?username=${NEXT_PUBLIC_DEV_TO_USERNAME}&per_page=5&state=fresh`
   );
   const articles = await res.json();
 
