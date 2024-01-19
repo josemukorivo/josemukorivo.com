13ef8b02c198c818ddc7d5e05368e6a63db95e7a
Joseph Mukorivo <josemukorivo@gmail.com>
2024-01-19 12:16:03 +0300

    Update GetInTouch and Hero components, delete unused images, and modify global styles

--- components/sections/GetInTouch/GetInTouch.tsx
+++ components/sections/GetInTouch/GetInTouch.tsx
@@ -2,7 +2,7 @@ import { Box, Button, Container, Text } from '@components/ui';
 import { Calendy } from '@components/common';
 
 export const GetInTouch = () => (
-  <Box className='mb-10 bg-slate-50  py-12 text-center dark:bg-[#0d1424]'>
+  <Box className='mb-10 bg-slate-100  py-12 text-center dark:bg-[#0d1424]'>
     <Container>
       <Text as='h2' className='mb-4' fontSize='4xl' align='center'>
         Get in touch
--- components/sections/Hero/Hero.tsx
+++ components/sections/Hero/Hero.tsx
@@ -79,14 +79,14 @@ export const Hero = () => {
         initial={{ opacity: 0.8 }}
         transition={{ duration: 0.6 }}
         animate={{ opacity: 1 }}
-        className='relative mt-[53px] h-[60vh] md:hidden bg-[url(/images/jose.jpg)] bg-cover bg-left-top'
+        className='relative mt-[53px] h-[60vh] md:hidden bg-[url(/images/joseph.jpg)] bg-cover bg-left-top'
       />
 
       <motion.div
         initial={{ opacity: 0.8 }}
         transition={{ duration: 0.6 }}
         animate={{ opacity: 1 }}
-        className='order-1 mt-0 hidden bg-slate-300 dark:bg-slate-800 h-full bg-[url(/images/jose.jpg)] bg-cover bg-center md:block'
+        className='order-1 mt-0 hidden bg-slate-300 dark:bg-slate-800 h-full bg-[url(/images/joseph.jpg)] bg-cover bg-center md:block '
       />
     </Box>
   );
